7a2096b398a2ee2331eb89c86a007313fb30a0f5
Stu W <stubillwhite@gmail.com>
2023-12-21 08:54:20 +0000

    Continuing hacking

--- src/advent_of_code_2023/day_8.clj
+++ src/advent_of_code_2023/day_8.clj
@@ -51,57 +51,100 @@
   (assoc input
          :ghosts (into []
                        (for [[k v] nodes :when (string/ends-with? k "A")]
-                         {:curr   k
-                          :path   []
-                          :len    nil
-                          :offset nil}))))
+                         {:curr       k
+                          :path       []
+                          :loop-start nil
+                          :exit       nil
+                          :loop-end   nil}))))
 
-(defn- next-ghost-state [nodes dir {:keys [curr path len] :as state}]
-  (if (nil? len)
+(defn- next-ghost-state [nodes dir {:keys [curr path loop-start exit loop-end] :as state}]
+  (if (nil? loop-end)
     (let [new-curr (get-in nodes [curr dir])
-          idx      (.indexOf path new-curr)]
-      (let [next-state {:curr   new-curr
-                        :path   (conj path curr)
-                        :len    (if (> idx 0) (- (inc (count path)) idx) len)
-                        :offset (if (> idx 0) idx)}]
-        (do (comment println next-state)
+          idx      (.indexOf path [new-curr dir])
+          new-path (conj path [curr dir])]
+      (let [next-state {:curr       new-curr
+                        :path       new-path
+                        :loop-start (if (> idx 0) idx loop-start)
+                        :exit       (if (string/ends-with? new-curr "Z") (count new-path) exit)
+                        :loop-end   (if (> idx 0) (count new-path) exit)}]
+        (do (println next-state)
             next-state)))
     state))
+
 
 (defn- next-state [{:keys [instr nodes curr ghosts] :as state}]
   (let [[dir & dirs] instr]
     (do
-      (comment println "---------------------------------------------------------------------")
+      (println "---------------------------------------------------------------------")
       (assoc state
              :instr  (lazy-cat dirs [dir])
              :ghosts (map (partial next-ghost-state nodes dir) ghosts)
              :curr   (get-in nodes [curr dir])))))
 
-(defn- gcd [a b]
-  (if (zero? b)
-    a
-    (recur b (mod a b))))
-
-(defn- lcm [a b]
-  (/ (* a b) (gcd a b)))
-
-(let [input problem-input]
+(let [input example-input-three]
   (let [state       (create-state (parse-input input))
         stream      (iterate next-state state)
-        final-state (first (drop-while (fn [{:keys [ghosts]}] (some (comp nil? :len) ghosts)) stream))]
-    (->> final-state
-         (:ghosts)
-         (map :len)
-         (reduce lcm)
-         (println))))
-
-(let [input problem-input]
-  (let [state       (create-state (parse-input input))
-        stream      (iterate next-state state)
-        final-state (first (drop-while (fn [{:keys [ghosts]}] (some (comp nil? :len) ghosts)) stream))]
+        final-state (first (drop-while (fn [{:keys [ghosts]}] (some (comp nil? :loop-start) ghosts)) stream))]
     (->> final-state
          (:ghosts)
          (clojure.pprint/pprint))
     ))
 
 ;; TODO: End of loops may not be ZZZ
+
+;; (let [input example-input]
+;;   (let [state       (create-state (parse-input input))
+;;         stream      (iterate next-state state)
+;;         final-state (first (drop-while (fn [{:keys [ghosts]}] (some (comp nil? :len) ghosts)) stream))]
+;;     (->> final-state
+;;          (:ghosts)
+;;          (map :len)
+;;          (reduce lcm)
+;;          (println))))
+
+;; clicks-to-exit
+;; exit, loop-length, loop-length, loop-length
+;; 2, 4, 6
+;; 3,    6
+
+(defn- coincident-points [xs ys]
+  (let [x (first xs)
+        y (first ys)]
+    (cond
+      (= x y) (cons x (lazy-seq (coincident-points (rest xs) (rest ys))))
+      (< x y) (coincident-points (rest xs) ys)
+      (> x y) (coincident-points xs (rest ys)))))
+
+(defn exit-points [{:keys [loop-start exit loop-end]}]
+  (let [start  exit
+        period (- loop-end loop-start)]
+    (iterate (fn [x] (+ x period)) start)))
+
+(let [a-exit 2
+      a-len  2
+      b-exit 3
+      b-len  3
+      stream (fn [start period] (iterate (fn [x] (+ x period)) start))]
+  (take 5 (drop 1000 (coincident-points (stream a-exit a-len)
+                             (stream b-exit b-len)))))
+
+
+(comment let [input example-input-three]
+  (let [state       (create-state (parse-input input))
+        stream      (iterate next-state state)
+        final-state (first (drop-while (fn [{:keys [ghosts]}] (some (comp nil? :loop-start) ghosts)) stream))]
+    (->> final-state
+         (:ghosts)
+         (clojure.pprint/pprint)
+         
+         )
+    ))
+
+
+
+(let [input example-input-three]
+  (let [state       (create-state (parse-input input))
+        stream      (iterate next-state state)
+        final-state (last (take 5 stream))]
+    (->> final-state
+         (clojure.pprint/pprint))))
